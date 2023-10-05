19e52b4ca3def593a2f50154a381f76a344bfd6b
catherine lim <jkvvhappy@gmail.com>
2023-10-05 10:37:33 +0800

    created clock using javascript

---
+++ index.js
@@ -0,0 +1,25 @@
+const indicator = (hour) => {
+  return (hour >= 12) ? "PM" : "AM"
+}
+
+const showTime = () => {
+  const date = new Date()
+
+  let hour = date.getHours()
+  hour = (hour < 10) ? "0" + hour : hour
+
+  let min = date.getMinutes()
+  min = (min < 10) ? "0" +min :min
+
+  let sec = date.getSeconds()
+  sec = (sec < 10) ? "0" +sec :sec
+
+  const getIndicator = indicator(hour)
+
+  const display = document.querySelector("#clockDisplay")
+  display.innerHTML = `${hour}: ${min}: ${sec} ${getIndicator}`
+
+  setTimeout(showTime, 1000)
+}
+
+showTime()
